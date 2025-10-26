b3230e3bcdb2f99ba7a9a86e0fb347e0c56267c8
Laíse <laise.soares321@gmail.com>
2025-10-26 11:41:45 -0300

    Recebendo os valores e entrada e mostrando como funciona o sistema de parcelas

---
+++ exercicios/exercicio_camisetas/exerciciocamisetas.py
@@ -0,0 +1,16 @@
+#CONSTRUINDO UM APP DE VENDAS DE CAMISETAS
+#SABE-SE QUE:
+# Se a quantidade de parcelas for menor que 4, o Juros será de 0% (0 / 100);
+# Se a quantidade de parcelas for igual ou maior que 4 e menor que 6, o Juros será de 4% (4 / 100); 
+# Se a quantidade de parcelas for igual ou maior que 6 e menor que 9, o Juros será de 8% (8 / 100);
+# Se a quantidade de parcelas for igual ou maior que 9 e menor que 13, o Juros será de 16% (16 / 100);
+# Se a quantidade de parcelas for igual ou maior que 13, o Juros será de 32% (32 / 100)
+print("Bem-vindos a loja da Laíse Soares!!!!!")
+valordDoPedido = float(input("Digite o valor do seu pedido: "))
+quantidadeDeParcelas = int(input("Digite a quantidade de parcelas que você quer: "))
+print("""Os juros se aplicam da seguinte maneira:
+Se a quantidade de parcelas for menor que 4, o Juros será de 0% (0 / 100);
+Se a quantidade de parcelas for igual ou maior que 4 e menor que 6, o Juros será de 4% (4 / 100);
+Se a quantidade de parcelas for igual ou maior que 6 e menor que 9, o Juros será de 8% (8 / 100);
+Se a quantidade de parcelas for igual ou maior que 9 e menor que 13, o Juros será de 16% (16 / 100);
+Se a quantidade de parcelas for igual ou maior que 13, o Juros será de 32% (32 / 100);""")
