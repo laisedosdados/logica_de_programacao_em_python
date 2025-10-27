8f461eeec2eb5bd0fc0c1ebcdb605f3ccfacda49
Laíse <laise.soares321@gmail.com>
2025-10-26 21:40:28 -0300

    Apresentando o restaurante e os preços, tamanhos e tipos de proteína

---
+++ exercicios/exercicio_marmitas/exericio_marmitas.py
@@ -0,0 +1,8 @@
+#RESTAURANTE DA LAÍSE SOARES
+print("Bem-vindos ao restaurante da Laíse Soares!!!")
+print("""
+===================Restaurante da Soares==================
+===Tamanho===Bife Acebolado (BA)===Filé de Frango (FF)===
+|------P-----|------R$ 16------|----------R$ 15----------|
+|------M-----|------R$ 18------|----------R$ 17----------|
+|------G-----|------R$ 22------|----------R$ 21----------|""")
