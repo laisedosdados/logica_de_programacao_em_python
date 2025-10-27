0d2d0a7872c72178e10db34c05d7b429149100a4
Laíse <laise.soares321@gmail.com>
2025-10-26 21:57:19 -0300

    Validando o tamanho e o tipo de carne inseridos

--- exercicios/exercicio_marmitas/exericio_marmitas.py
+++ exercicios/exercicio_marmitas/exericio_marmitas.py
@@ -6,11 +6,10 @@ print("""
 |------P-----|------R$ 16------|----------R$ 15----------|
 |------M-----|------R$ 18------|----------R$ 17----------|
 |------G-----|------R$ 22------|----------R$ 21----------|""")
-#RECEBENDO O SABOR E TAMANHO DESEJADOS
+#RECEBENDO O SABOR E TAMANHO DESEJADOS E VALIDANDO-OS
 sabor = input("Digite o sabor desejado (BA/FF): ").lower()
 while sabor.upper() != "BA" and sabor.upper() != "FF":
     sabor = input("Sabor inválido!Tente novamente: ")
 tamanho = input("Digite o tamanho desejado(P/M/G): ").lower()
 while tamanho.upper() != "P" and tamanho.upper() != "M"and tamanho.upper() != "G":
     tamanho = input("Tamanho inválido!Tente novamente: ")
-    
